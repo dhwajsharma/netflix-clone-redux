f0989b9b0d0a2e79f0d1f35a9654f14fdecabe44
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-29 23:16:26 +0530

    cleaning up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8ugelZmuFq8SXw1QHefnVksfAIhmwbkU",
+    authDomain: "netflix-clone-redux-8e431.firebaseapp.com",
+    projectId: "netflix-clone-redux-8e431",
+    storageBucket: "netflix-clone-redux-8e431.appspot.com",
+    messagingSenderId: "713143126870",
+    appId: "1:713143126870:web:5429b634ce565c4c4b24f7"
+};
+
