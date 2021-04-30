5a5c14fbeaa9eda8b3135a5393b956bdffaa2c37
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-30 14:51:28 +0530

    Built Rows

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -20,8 +20,8 @@ const Nav = () => {
     return (
         <div className={`nav ${show && "nav_black"}`}>
             <div className="nav_contents">
-                <img className="nav_logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
-                <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
+                <img loading="lazy" className="nav_logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+                <img loading="lazy" className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
             </div>
         </div >
     )
